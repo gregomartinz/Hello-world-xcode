480e6e505aaa232d197f579844a836b2c9c98a22
g304 DIT UPM <g304@l140.lab.dit.upm.es>
2015-09-10 12:22:25 +0200

    Initial Commit

---
+++ Hola mundo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hola mundo
+//
+//  Created by g304 DIT UPM on 10/9/15.
+//  Copyright (c) 2015 g304 DIT UPM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
