8a0eec112bbe070e6b57d7363557aedcc591019c
Gregorio Martínez Luque <Grego@MacBook-Pro-de-Gregorio.local>
2016-01-17 01:03:14 +0100

    First commit

--- Hola mundo/ViewController.swift
+++ Hola mundo/ViewController.swift
@@ -7,9 +7,19 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    
+    
+
+    @IBOutlet weak var msg: UILabel!
+
+    @IBOutlet weak var map: MKMapView!
+    
+    @IBOutlet weak var slider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +30,37 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func holaPressed(sender: UIButton) {
+        msg.text = "Hola"
+        map.mapType = MKMapType.Hybrid
+        
+        slider.setValue(0.5, animated: true)
+    }
 
+
+    @IBAction func mundoPressed(sender: UIButton) {
+        self.msg.text = "Mundo"
+        let span = MKCoordinateSpanMake(0.05, 0.05)
+        let center = CLLocationCoordinate2DMake(40.452842, -3.726173)
+        let reg = MKCoordinateRegionMake(center, span)
+        
+        map.setRegion(reg, animated: true)
+        map.mapType = MKMapType.Satellite
+    }
+    
+    @IBAction func sliderMoved(sender: UISlider) {
+        msg.alpha = CGFloat(sender.value)
+    }
+    
+    
+    @IBAction func piramides(sender: AnyObject) {
+        let span = MKCoordinateSpanMake(0.05, 0.05)
+        let center = CLLocationCoordinate2DMake(29.97729620916011, 31.132495500000005)
+        let reg = MKCoordinateRegionMake(center, span)
+        
+        map.setRegion(reg, animated: true)
+    }
+ 
+    
 }
 
